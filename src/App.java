b6757e6929b25bbf97525fcdcf25b08c90aeddab
Marcos Feldick <marcosefeldick@gmail.com>
2022-08-05 05:32:00 -0300

    Added scanner for user input

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -8,6 +10,14 @@ public class App {
                 { '-', '+', '-', '+', '-' },
                 { ' ', '|', ' ', '|', ' ' } };
         printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9):");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
+
+        scan.close();
     }
 
     public static void printGameBoard(char[][] gameBoard) {
