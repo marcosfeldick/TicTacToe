67ebc8e3800f8328f44c668d6934679d5e37159e
Marcos Feldick <marcosefeldick@gmail.com>
2022-08-05 05:43:25 -0300

    Created switch statement to print the user's input to the board

--- src/App.java
+++ src/App.java
@@ -18,6 +18,37 @@ public class App {
         System.out.println(pos);
 
         scan.close();
+
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+        printGameBoard(gameBoard);
     }
 
     public static void printGameBoard(char[][] gameBoard) {
